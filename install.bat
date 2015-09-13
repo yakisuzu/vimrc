cb3480b6c62e323466f7df8792b663a01cef926d
yakisuzu <yakisuzu@gmail.com>
2015-09-13 20:19:06 +0900

    append win setting

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo offf
+pushd %~dp0
+
+echo --------------------
+echo init bash
+call .¥init_script¥bash.bat
+
+echo --------------------
+echo init git
+call .¥init_script¥git.bat
+
+echo --------------------
+echo init vim
+call .¥init_script¥vim.bat
+
+popd
+pause
