acd3e173a6dddf69bd1ff0b944977f6e888d60bc
yakisuzu <yakisuzu@gmail.com>
2015-09-15 20:16:29 +0900

    fix script bat

--- script/get_env_path.bat
+++ script/get_env_path.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH','Machine')|grep -e 's/;/\n/g'"
+powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH','Machine')|sed -e 's/;/\n/g'"
