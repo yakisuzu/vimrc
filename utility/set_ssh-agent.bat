2d3f5e755f68eade012f430cfdf4896271d21b06
yakisuzu <yakisuzu@gmail.com>
2017-03-03 13:58:36 +0900

    ssh-agent setting for cmd

---
+++ utility/set_ssh-agent.bat
@@ -0,0 +1,13 @@
+@echo off
+pushd %~dp0
+
+if not "%SSH_AGENT_PID%" == "" goto end
+for /f "eol=; tokens=1,2 delims==;" %%1 in ('ssh-agent.exe') do (
+ if "%%1" == "SSH_AUTH_SOCK" set SSH_AUTH_SOCK=%%2
+ if "%%1" == "SSH_AGENT_PID" set SSH_AGENT_PID=%%2
+)
+ssh-add %HOME%\.ssh\id_rsa 2>nul
+
+:end
+popd
+
