f1cdf76f55478a09bf5771be269009d3e75aaa30
yakisuzu <yakisuzu@gmail.com>
2017-03-27 16:13:11 +0900

    dockerfile centos

---
+++ Docker/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pushd `dirname $0` >/dev/null
+
+cp -r ~/.ssh ./ssh
+docker build -t workcent:0.0.1 .
+
+mkdir -p work
+docker run -it --name=workcent -v work://work workcent:0.0.1
+
+popd >/dev/null
+
