9a9cf7ebe2adc095e6b34badc5f2f65a61789629
yakisuzu <yakisuzu@gmail.com>
2017-10-06 18:37:34 +0900

    path setting

--- utility/get_env_path.bat
+++ utility/get_env_path.bat
@@ -1,9 +1,9 @@
 @echo off
-echo ====================
 echo Machine setting
-powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::Machine).Replace(';',\"`n\")"
+echo ---------------
+powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::Machine) -split \";\""
 echo.
-echo ====================
 echo User setting
-powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::User).Replace(';',\"`n\")"
+echo ------------
+powershell -ExecutionPolicy unrestricted -Command "[Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::User) -split \";\""
 
---
+++ utility/set_path.ps1
@@ -0,0 +1,45 @@
+#$HOME setting
+[Environment]::SetEnvironmentVariable('HOME', [Environment]::GetEnvironmentVariable('USERPROFILE', [EnvironmentVariableTarget]::Process), [EnvironmentVariableTarget]::User)
+
+$SystemDrive = [Environment]::GetEnvironmentVariable('SystemDrive', [EnvironmentVariableTarget]::Process)
+$ProgramFiles = [Environment]::GetEnvironmentVariable('ProgramFiles', [EnvironmentVariableTarget]::Process)
+
+Class App {
+  [string]$Name
+  [string]$Path
+
+  App([string]$Name, [string]$Path) {
+    $this.Name = $Name
+    $this.Path = $Path
+  }
+}
+
+$Apps = [App[]](
+  [App]::new("git", $ProgramFiles + "\Git\cmd"),
+  [App]::new("OpenSSH", $ProgramFiles + "\OpenSSH-Win64"),
+  [App]::new("vim", $ProgramFiles + "\vim80-kaoriya-win64"),
+  [App]::new("ConEmu", $SystemDrive + "\ConEmu"),
+  [App]::new("msys2/root", $SystemDrive + "\msys64"),
+  [App]::new("msys2/bin", $SystemDrive + "\msys64\usr\bin"),
+  #[App]::new("docker", $ProgramFiles + "\Docker Toolbox"),
+  [App]::new("dotfiles", $HOME + "\dotfiles\bin")
+)
+
+Write-Output -InputObject $Apps
+Write-Output ""
+
+[string[]] $AddAppPaths = @()
+$EnvironmentPaths = [Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::User) -split ";"
+$Apps | % {
+  if(!($EnvironmentPaths -contains $_.Path)){
+    $AddAppPaths += $_.Path
+  }
+}
+
+$AfterPaths = $AddAppPaths + $EnvironmentPaths
+[Environment]::SetEnvironmentVariable('PATH', ($AfterPaths -join ";"), [EnvironmentVariableTarget]::User)
+
+Write-Output AfterPaths
+Write-Output ----------
+Write-Output $AfterPaths
+
