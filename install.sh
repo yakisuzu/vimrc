74a18d85b9c8c7c6492c2b8c4b507d6546303565
yakisuzu <yakisuzu@gmail.com>
2019-05-21 14:28:45 +0900

    install.sh replace

--- install.sh
+++ install.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
-pushd `dirname $0` >/dev/null
+pushd `dirname $0` > /dev/null
+
+# ------------------------------
+function LINK_OVERRIDE(){
+  SRC_PATH="$1"
+  DEST_PATH="$2"
+
+  [ -L "$DEST_PATH" ] \
+    && rm "$DEST_PATH" \
+    && echo "rm $DEST_PATH"
+
+  echo "make $DEST_PATH"
+  ln -s "$SRC_PATH" "$DEST_PATH"
+}
+
+# ------------------------------
+function COPY_NOT_EXISTS(){
+  SRC_PATH="$1"
+  DEST_PATH="$2"
+
+  [ -e "$DEST_PATH" ] \
+    && echo "exists $DEST_PATH" \
+    && return 0
+
+  echo "cp $DEST_PATH"
+  cp "$SRC_PATH" "$DEST_PATH"
+}
 
 echo --------------------
 echo init bash
-. ./script/bash.sh
+LINK_OVERRIDE "$PWD/bash/.bash_profile" "$HOME/.bash_profile"
+LINK_OVERRIDE "$PWD/bash/.bashrc" "$HOME/.bashrc"
+COPY_NOT_EXISTS "$PWD/git/.bashrc_local" "$HOME/.bashrc_local"
+LINK_OVERRIDE "$PWD/bash/.inputrc" "$HOME/.inputrc"
+LINK_OVERRIDE "$PWD/bash/.tmux.conf" "$HOME/.tmux.conf"
 
 echo --------------------
 echo init git
-. ./script/git.sh
+LINK_OVERRIDE "$PWD/git/.gitconfig" "$HOME/.gitconfig"
+LINK_OVERRIDE "$PWD/git/.gitattributes" "$HOME/.gitattributes"
+COPY_NOT_EXISTS "$PWD/git/.gitconfig_local" "$HOME/.gitconfig_local"
+COPY_NOT_EXISTS "$PWD/git/.gitignore_local" "$HOME/.gitignore_local"
 
 echo --------------------
 echo init vim
-. ./script/vim.sh
+LINK_OVERRIDE "$PWD/vim/gvimrc.vim" "$HOME/_gvimrc"
+LINK_OVERRIDE "$PWD/vim/vimrc.vim" "$HOME/_vimrc"
+LINK_OVERRIDE "$PWD/vim/.vim/after" "$HOME/.vim/after"
+LINK_OVERRIDE "$PWD/vim/.vim/ftdetect" "$HOME/.vim/ftdetect"
+LINK_OVERRIDE "$PWD/vim/.vim/syntax" "$HOME/.vim/syntax"
 
 echo --------------------
 echo init lint
-. ./script/lint.sh
+LINK_OVERRIDE "$PWD/lint/.eslintrc.json" "$HOME/.eslintrc.json"
 
-popd >/dev/null
-
+unset LINK_OVERRIDE
+unset COPY_NOT_EXISTS
+popd > /dev/null
