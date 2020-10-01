ce23828262ff09bd0fb82af43942b37be6564163
yakisuzu <yakisuzu@gmail.com>
2020-10-02 01:13:13 +0900

    anyenv updateの整形

--- bin/update_brew_anyenv.sh
+++ bin/update_brew_anyenv.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 
 brew upgrade
-brew cask upgrade
+brew upgrade --cask
 anyenv update
 
 # conf
-echo "#############################"
-echo "goenv versions"
+echo ""
+echo "-----------------------------"
+echo "- goenv versions"
 goenv versions
-echo "goenv available"
+echo "- goenv available"
 goenv install -l | tail -3
 
-echo "#############################"
-echo "jenv versions"
+echo ""
+echo "-----------------------------"
+echo "- jenv versions"
 jenv versions
-echo "jenv available"
-/usr/libexec/java_home -V 2>&1 | tail -n +2 | head -n -2
+echo "- jenv available"
+/usr/libexec/java_home -V 2>&1 | tail -n +2 | head -n -2 | sed -e 's/^  //'
 
-echo "#############################"
-echo "nodenv versions"
+echo ""
+echo "-----------------------------"
+echo "- nodenv versions"
 nodenv versions
-echo "nodenv available"
-nodenv install -l | grep '^8\.' | tail -3
-nodenv install -l | grep '^10\.' | tail -3
-nodenv install -l | grep '^12\.' | tail -3
+echo "- nodenv available"
+nodenv install -l | grep '^12\.' | tail -3 | sed -e 's/^/  /'
 
-echo "#############################"
-echo "pyenv versions"
+echo ""
+echo "-----------------------------"
+echo "- pyenv versions"
 pyenv versions
-echo "pyenv available"
-pyenv install -l | grep '^  2' | tail -3
-pyenv install -l | grep '^  3' | tail -3
+echo "- pyenv available"
+pyenv install -l | grep '^  2.[0-9]\+.[0-9]\+$' | tail -3
+pyenv install -l | grep '^  3.[0-9]\+.[0-9]\+$' | tail -3
 
-echo "#############################"
-echo "rbenv versions"
+echo ""
+echo "-----------------------------"
+echo "- rbenv versions"
 rbenv versions
-echo "rbenv available"
-rbenv install -l | grep '^2' | tail -3
+echo "- rbenv available"
+rbenv install -l 2>/dev/null | grep '^2' | tail -3 | sed -e 's/^/  /'
+
